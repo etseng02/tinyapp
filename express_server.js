b4a867af219e9bd62594eef4c2d667b3e2c59086
Eddie Tseng <eddietseng@outlook.com>
2019-09-04 19:51:25 +0000

    Added register functionality and built page for login

--- express_server.js
+++ express_server.js
@@ -15,12 +15,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 app.post("/urls", (req, res) => {
   let newURL = generateRandomString();
   urlDatabase[newURL] = req.body.longURL
-  console.log(req.body);  // Log the POST request body to the console
+  //console.log(req.body);  // Log the POST request body to the console
   //res.send();         // Respond with 'Ok' (we will replace this)
-  console.log(urlDatabase)
+  //console.log(urlDatabase)
   res.redirect("/urls/" + newURL)
 });
 
@@ -35,27 +48,50 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  console.log(req.body.username);
-  res.cookie("username",req.body.username);
-  console.log('cookie created successfully');
+  //console.log(req.body.username);
+  //res.cookie("username",req.body.username);
+  //console.log('cookie created successfully');
   res.redirect('/urls');
 })
 
 app.post('/logout', (req, res) => {
-  console.log(req.body.username);
-  res.clearCookie("username",req.body.username);
-  console.log('this button works');
+  //console.log(req.body.username);
+  res.clearCookie("user_id",req.body.user_id);
+  //console.log('this button works');
   res.redirect('/urls');
 })
 
+app.post('/register', (req, res) => {
+  //console.log(req.body.email, req.body.password)
+  if (req.body.email == "" || req.body.password == ""){
+    res.status(400).send('Error Code: 400 Bad Request')
+  } else {
+    let id = generateRandomString()
+    createUser(id, req.body.email, req.body.password)
+    res.cookie("user_id", id);
+    res.redirect('/urls');
+  }
+})
+
+app.get('/register', (req, res) => {
+  let templateVars = { username: userLookup(req.cookies["user_id"]), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("register.ejs", templateVars);
+})
+
+app.get('/login', (req, res) => {
+  let templateVars = { username: userLookup(req.cookies["user_id"]), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("login.ejs", templateVars);
+})
+
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { username: userLookup(req.cookies["user_id"]) };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  let templateVars = { username: userLookup(req.cookies["user_id"]), urls: urlDatabase };
   res.render("urls_index", templateVars);
+  //console.log("this is the result of userlookup function: " + userLookup(req.cookies["user_id"]))
 });
 
 app.get("/urls.json", (req, res) => {
@@ -64,7 +100,7 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
+  //console.log(longURL);
   if (longURL) {
     res.redirect(longURL);
   } else {
@@ -73,7 +109,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { username: userLookup(req.cookies["user_id"]), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   //const longURL = urlDatabase[req.params.shortURL];
   res.render("urls_show", templateVars);
 });
@@ -90,3 +126,28 @@ function generateRandomString() {
   }
   return newShortURL;
 };
+
+const createUser = function(id, email, password){
+  users[id] = {
+    id: id,
+    email: email,
+    password: password
+  }
+}
+
+const userLookup = function(id){
+  for (name in users) {
+    //console.log (name);
+    if (name === id) {
+      return users[id].email
+    }
+  }
+}
+
+/*class createUser {
+  constructor(id, email, password) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+  }
+}*/
